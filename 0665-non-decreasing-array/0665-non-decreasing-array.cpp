a9f621f4d80c799727faa2dfad1d0b3c72558554
Abhishek <abhisheksinghcsanand@gmail.com>
2023-08-04 14:08:03 +0530

    Time: 20 ms (90.38%), Space: 27.1 MB (28.04%) - LeetHub

--- 0665-non-decreasing-array/0665-non-decreasing-array.cpp
+++ 0665-non-decreasing-array/0665-non-decreasing-array.cpp
@@ -5,7 +5,7 @@ public:
         for(int i=1;i<n;i++){
             if(nums[i]<nums[i-1]){
                 if(cnt == 1) return false;
-                if(i<2 || nums[i-2]<=nums[i]){
+                if(i==1 || nums[i-2]<=nums[i]){
                     nums[i-1]=nums[i];
                 }
                 else{
